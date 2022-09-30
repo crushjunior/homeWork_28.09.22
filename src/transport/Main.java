093cabf877317f8cb564a219d895e6613a5bd7f0
crushjunior <94167663+crushjunior@users.noreply.github.com>
2022-09-30 13:50:34 +0300

    task 1.1

--- src/transport/Main.java
+++ src/transport/Main.java
@@ -1,7 +1,30 @@
 package transport;
 
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
+        Car lada = new Car("Lada", "Granta", 160, 1.7, "Жёлтый", 2015, "Россия");
+        lada.setGear("Автомат");
+        lada.setRegNumber("e123rr1223");
+        System.out.println(lada.isCorrectNumber());
+        lada.changeTyre();
+        Car audi = new Car("Audi", "A8 50 L TDI quattro", 240, 3.0, "Чёрный", 2020, "Германия");
+        audi.setKey(new Car.Key(true,true));
+        Car bmw = new Car("BMW", "Z8", 260, 3.0, "Чёрный", 2021, "Германия");
+        bmw.setInsurance(new Car.Insurance(LocalDate.now(), 9_000, "1244"));
+        Car kia = new Car("KIA", "Sportage", 200, 2.4, "Красный", 2018, "Южная Корея");
+        Car hyundai = new Car("Hyundai", "Avante", 180, 1.6, "Оранжевый", 2016, "Южная Корея");
 
+
+
+        lada.carInfo();
+        audi.carInfo();
+        bmw.carInfo();
+        kia.carInfo();
+        hyundai.carInfo();
+        System.out.println();
+        bmw.getInsurance().checkExpireDate();
+        bmw.getInsurance().checkNumber();
     }
 }
---
+++ src/transport/Transport.java
@@ -0,0 +1,84 @@
+package transport;
+
+public class Transport {
+    private final String brand;
+    private final String model;
+    private final int productionYear;
+    private final String productionCountry;
+    private String color;
+    private int maxSpeed;
+
+    public Transport(String brand, String model, int productionYear, String productionCountry, String color, int maxSpeed) {
+        if (brand != null && !brand.isEmpty() && !brand.isBlank()) {
+            this.brand = brand;
+        } else {
+            this.brand = "default";
+        }
+
+        if (model != null && !model.isEmpty() && !model.isBlank()) {
+            this.model = model;
+        } else {
+            this.model = "default";
+        }
+
+        if (productionYear <= 0) {
+            this.productionYear = 2000;
+        } else {
+            this.productionYear = productionYear;
+        }
+
+        if (productionCountry != null && !productionCountry.isEmpty() && !productionCountry.isBlank()) {
+            this.productionCountry = productionCountry;
+        } else {
+            this.productionCountry = "default";
+        }
+
+        setColor(color);
+
+        setMaxSpeed(maxSpeed);
+    }
+
+    public Transport(String brand, String model, int productionYear, String productionCountry) {
+        this(brand, model, productionYear, productionCountry, "белый", 100);
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getProductionYear() {
+        return productionYear;
+    }
+
+    public String getProductionCountry() {
+        return productionCountry;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        if (color == null) {
+            this.color = "белый";
+        } else {
+            this.color = color;
+        }
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public void setMaxSpeed(int maxSpeed) {
+        if (maxSpeed <= 0) {
+            this.maxSpeed = 100;
+        } else {
+            this.maxSpeed = maxSpeed;
+        }
+    }
+}
